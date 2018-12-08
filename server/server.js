cf0c884f29fe903ceac771ff86232170ce4f4f80
juwanpetty <jchpetty@gmail.com>
2018-12-08 00:21:21 -0500

    Inital commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
